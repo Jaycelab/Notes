ba6f1770869d01a0e4dc934bddaaf607e898c673
Jason Alvarado <jayxalvarado@gmail.com>
2024-06-11 12:55:59 -0700

    spelling cleanup

--- dsaa/dsaa-examples.ts
+++ dsaa/dsaa-examples.ts
@@ -1,7 +1,7 @@
 // Per every 1 more unit of string, 1 more loops has to occur
 // Look for loops to determine a Big O
 // Constant drops as the algorithm grows
-// The vavlue of squared N becomes larger as the value of
+// The value of squared N becomes larger as the value of
 // N increase in number
 function sum_char_codes(n: string): number {
   let sum = 0;
@@ -18,7 +18,7 @@ function sum_char_codes(n: string): number {
 // Example below without a Capital E, would go through the entire alphabet
 // It would still be considered O(N) as the position of E would not matter
 
-function sum_char_codes(n: string): number {
+function sum_char_codesA(n: string): number {
   let sum = 0;
   for (let i = 0; i < n.length; ++i) {
     const charCode = n.charCodeAt(i);
